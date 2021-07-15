552b47cb1e900c5e19007c50e008e65a289f7374
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-07-16 02:30:29 +0900

    update nav, about, css, detailpage

---
+++ movie-app/src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  return (
+    <div className="about__container">
+      <span>Checking the ToDO LIST</span>
+    </div>
+  );
+}
+
+export default About;
