c7f1dbc042950fb8d71b8e02934e3632c2a3df77
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-07-15 19:55:38 +0900

    set State preState Study

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,72 +1,27 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
-function Apple({ name, picture, rating }) {
-  console.log(name);
-  return (
-    <div>
-      <h1>I like {name} !!</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-Apple.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-const iPhoneList = [
-  {
-    id: 1,
-    name: "iPad",
-    image:
-      "https://image.cnet.co.kr/2020/09/16/5ac9389cf55eaec00460033def8d0137-770xAAA.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "AppleWatch",
-    image:
-      "https://image.zdnet.co.kr/2020/09/16/b95bb43226c7817993b1acc6383d7dbc.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "MacBook",
-    image:
-      "https://blog.kakaocdn.net/dn/cbBCEr/btqH4tmFDey/xZqIQbKQPxkTSFZdbzBbnk/img.jpg",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "iPhone",
-    image:
-      "https://support.apple.com/library/content/dam/edam/applecare/images/ko_KR/iOS/ios14-iphone-11pro-setup-hello-select-language.jpg",
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    name: "AirPods",
-    image:
-      "https://funshop1.akamaized.net/products/0000076114/637099241290463260.jpg",
-    rating: 5.5,
-  },
-];
-function App() {
-  return (
-    <div>
-      {iPhoneList.map((item) => (
-        <Apple
-          key={item.id}
-          name={item.name}
-          picture={item.image}
-          rating={item.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("Add");
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  sub = () => {
+    console.log("Sub");
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.sub}>Sub</button>
+      </div>
+    );
+  }
 }
 
 export default App;
